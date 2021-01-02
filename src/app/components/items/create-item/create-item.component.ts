e79b5403e4fbe9601404c9c5a365e18407f14e13
Ringo Rohe <hallo@ringorohe.de>
2021-01-02 22:40:19 +0100

    adding stand-alone create-item Form

--- src/app/components/items/create-item/create-item.component.ts
+++ src/app/components/items/create-item/create-item.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgPopupsService } from 'ng-popups';
+import { ToastrService } from 'ngx-toastr';
+import { ZeugItem } from 'src/app/models/ZeugItem';
+import { ZeugType } from 'src/app/models/ZeugType';
+import { ItemsService } from 'src/app/services/items.service';
+import { TypesService } from 'src/app/services/types.service';
+import { ItemFormComponent } from '../shared/item-form/item-form.component';
 
 @Component({
   selector: 'app-create-item',
   templateUrl: './create-item.component.html',
-  styleUrls: ['./create-item.component.scss']
+  styleUrls: ['./create-item.component.scss'],
 })
 export class CreateItemComponent implements OnInit {
+  types: ZeugType[];
+  items: ZeugItem[];
 
-  constructor() { }
+  @ViewChild(ItemFormComponent) formComponent: ItemFormComponent;
+
+  constructor(
+    private typesService: TypesService,
+    private itemsService: ItemsService,
+    private toast: ToastrService,
+    private popups: NgPopupsService
+  ) {}
 
   ngOnInit(): void {
+    this.types = this.typesService.types;
+    this.typesService.typesChanged.subscribe((response) => {
+      this.types = response;
+    });
+    this.items = this.itemsService.items;
+    this.itemsService.itemsChanged.subscribe((response) => {
+      this.items = response;
+    });
   }
 
+  onCreateFormSubmit = (data) => {
+    let newItem = ZeugItem.fromObject(data);
+    let promise = this.itemsService.createItem(newItem);
+
+    promise.then(
+      (result) => {
+        this.formComponent.reset();
+        this.toast.success('Item created.');
+      },
+      (error) => {
+        this.toast.error('Whoops. Item not saved.');
+      }
+    );
+  };
 }
